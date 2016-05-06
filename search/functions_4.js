f846ca87d4ba899da1afaadcea09140e0a6ba640
Taylor C. Richberger <Taywee@gmx.com>
2016-05-06 15:29:25 -0600

    improve generation and handling

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['getchilddescriptions',['GetChildDescriptions',['../classargs_1_1Group.html#aa59bec7c580d0ea7288477c4ffeed35f',1,'args::Group']]],
+  ['getchilddescriptions',['GetChildDescriptions',['../classargs_1_1Group.html#a108204e32db7dd5b31dc00d03daa8990',1,'args::Group']]],
   ['getnextpos',['GetNextPos',['../classargs_1_1Group.html#a93fa3460026c13e264388d314068fdfe',1,'args::Group']]],
-  ['getoptionstrings',['GetOptionStrings',['../classargs_1_1Matcher.html#aa568c30ccb60200be5f0a0549b222ecc',1,'args::Matcher']]],
+  ['getoptionstrings',['GetOptionStrings',['../classargs_1_1Matcher.html#aa568c30ccb60200be5f0a0549b222ecc',1,'args::Matcher::GetOptionStrings(const std::string &amp;shortPrefix, const std::string &amp;longPrefix) const '],['../classargs_1_1Matcher.html#a5b32f33973b5e52895f35e57558709f3',1,'args::Matcher::GetOptionStrings(const std::string &amp;shortPrefix, const std::string &amp;longPrefix, const std::string &amp;name, const std::string longSeparator) const ']]],
   ['getposnames',['GetPosNames',['../classargs_1_1Group.html#a85382243f31e08ef0ce5df807d82d102',1,'args::Group']]]
 ];
