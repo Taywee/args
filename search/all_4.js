cb7682e120c69937a200089a66ac5dd59ba67ea2
Taylor C. Richberger <Taywee@gmx.com>
2016-06-14 19:44:45 -0600

    update and bump versions

--- search/all_4.js
+++ search/all_4.js
@@ -3,5 +3,6 @@ var searchData=
   ['eachgroupindent',['eachgroupindent',['../structargs_1_1ArgumentParser_1_1HelpParams.html#a39e31bcfc82a687ed8290783546ffd05',1,'args::ArgumentParser::HelpParams']]],
   ['eitherflag',['EitherFlag',['../structargs_1_1EitherFlag.html',1,'args']]],
   ['epilog',['Epilog',['../classargs_1_1ArgumentParser.html#a1ff0ec0f671be914bf2dd058b163f8e1',1,'args::ArgumentParser::Epilog() const '],['../classargs_1_1ArgumentParser.html#a528b21b78c561266322c80aa9719bc65',1,'args::ArgumentParser::Epilog(const std::string &amp;epilog)']]],
-  ['error',['Error',['../classargs_1_1Error.html',1,'args']]]
+  ['error',['Error',['../classargs_1_1Error.html',1,'args']]],
+  ['extraerror',['ExtraError',['../classargs_1_1ExtraError.html',1,'args']]]
 ];
--- search/classes_3.js
+++ search/classes_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['eitherflag',['EitherFlag',['../structargs_1_1EitherFlag.html',1,'args']]],
-  ['error',['Error',['../classargs_1_1Error.html',1,'args']]]
+  ['error',['Error',['../classargs_1_1Error.html',1,'args']]],
+  ['extraerror',['ExtraError',['../classargs_1_1ExtraError.html',1,'args']]]
 ];
