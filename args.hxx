11016849db5fdeceb5bf3550ac7f0ada1ce2419c
Taylor C. Richberger <taywee@gmx.com>
2022-12-29 09:49:45 -0700

    make cmake install also install version file

--- args.hxx
+++ args.hxx
@@ -33,10 +33,10 @@
 #ifndef ARGS_HXX
 #define ARGS_HXX
 
-#define ARGS_VERSION "6.4.2"
+#define ARGS_VERSION "6.4.3"
 #define ARGS_VERSION_MAJOR 6
 #define ARGS_VERSION_MINOR 4
-#define ARGS_VERSION_PATCH 2
+#define ARGS_VERSION_PATCH 3
 
 #include <algorithm>
 #include <iterator>
--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile
 
 class ArgsConan(ConanFile):
     name = "args"
-    version = "6.4.2"
+    version = "6.4.3"
     url = "https://github.com/Taywee/args"
     description = "A simple header-only C++ argument parser library."
     license = "MIT"
