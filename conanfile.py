6c223d46dbb1db72320a93404552117b12d0e7ab
Taylor C. Richberger <taylor@axfive.net>
2026-02-10 17:44:21 -0700

    bump version to v6.4.8

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile
 
 class ArgsConan(ConanFile):
     name = "args"
-    version = "6.4.7"
+    version = "6.4.8"
     url = "https://github.com/Taywee/args"
     description = "A simple header-only C++ argument parser library."
     license = "MIT"
