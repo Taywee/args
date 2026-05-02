8b4291c9d221bc30b968a5fa8824c19ccab2dd9f
metsw24-max <metsw24@gmail.com>
2026-05-02 10:54:01 +0530

    Harden ParseCLI against null argv and invalid argc to prevent undefined behavior

---
+++ test/parsecli_zero_argc.cxx
@@ -0,0 +1,18 @@
+/* Copyright (c) Taylor Richberger <taylor@axfive.net>
+ * This code is released under the license described in the LICENSE file
+ */
+
+#include "test_common.hxx"
+
+#include <args.hxx>
+
+#include "test_helpers.hxx"
+
+int main()
+{
+    args::ArgumentParser parser("This is a test program.");
+
+    const bool result = parser.ParseCLI(0, nullptr);
+    test::require(result);
+    return 0;
+}
