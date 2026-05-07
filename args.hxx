745abe32469a9f576f85ff1db9298f549878c554
Taylor Richberger <taylor@axfive.net>
2026-05-07 15:03:18 -0600

    bump version to 6.4.16

--- args.hxx
+++ args.hxx
@@ -37,10 +37,10 @@
 #undef min
 #undef max
 
-#define ARGS_VERSION "6.4.15"
+#define ARGS_VERSION "6.4.16"
 #define ARGS_VERSION_MAJOR 6
 #define ARGS_VERSION_MINOR 4
-#define ARGS_VERSION_PATCH 15
+#define ARGS_VERSION_PATCH 16
 
 #include <algorithm>
 #include <iterator>
