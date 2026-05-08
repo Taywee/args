accfdf0787b03bba7e83a4f2be0df3a694cf3342
metsw24-max <metsw24@gmail.com>
2026-05-08 11:35:03 +0530

    Harden noexcept parsing against post-failure state mutation

---
+++ test/noexcept_parser_failure_state.cxx
@@ -0,0 +1,53 @@
+/* Copyright (c) Taylor Richberger <taylor@axfive.net>
+ * This code is released under the license described in the LICENSE file
+ */
+
+#define ARGS_NOEXCEPT
+#include "test_common.hxx"
+
+#include <args.hxx>
+
+#include "test_helpers.hxx"
+
+int main()
+{
+    {
+        args::ArgumentParser parser("Test command");
+        args::NargsValueFlag<int> nums(parser, "NUMS", "test", {'n', "nums"}, {1, 2}, {42});
+
+        parser.ParseArgs(std::vector<std::string>{"--nums", "abc"});
+        test::require(parser.GetError() == args::Error::Parse);
+        test::require((*nums == std::vector<int>{}));
+    }
+
+    {
+        args::ArgumentParser parser("Test command");
+        args::ValueFlagList<int> vals(parser, "VAL", "test", {'v', "vals"}, {11});
+
+        parser.ParseArgs(std::vector<std::string>{"--vals", "abc"});
+        test::require(parser.GetError() == args::Error::Parse);
+        test::require((*vals == std::vector<int>{}));
+    }
+
+    {
+        args::ArgumentParser parser("Test command");
+        args::Positional<int> pos(parser, "POS", "test", 123);
+
+        parser.ParseArgs(std::vector<std::string>{"abc"});
+        test::require(parser.GetError() == args::Error::Parse);
+        test::require(*pos == 123);
+        test::require_false(static_cast<bool>(pos));
+    }
+
+    {
+        args::ArgumentParser parser("Test command");
+        args::PositionalList<int> poslist(parser, "POSLIST", "test", {99});
+
+        parser.ParseArgs(std::vector<std::string>{"abc"});
+        test::require(parser.GetError() == args::Error::Parse);
+        test::require((*poslist == std::vector<int>{}));
+        test::require_false(static_cast<bool>(poslist));
+    }
+
+    return 0;
+}
