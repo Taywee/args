5447fc868cb92667728e9996895f150f0aab1bcd
jmestwa-coder <jmestwa@gmail.com>
2026-05-26 20:59:20 +0530

    Align ARGS_NOEXCEPT completion behavior with throwing mode

--- test/noexcept_completion.cxx
+++ test/noexcept_completion.cxx
@@ -21,5 +21,25 @@ int main()
     p.ParseArgs(std::vector<std::string>{"--completion", "bash", "1", "test", "-"});
     test::require(p.GetError() == args::Error::Completion);
     test::require(args::get(c) == "-f\n-b");
+
+    args::ArgumentParser p2("parser");
+    args::CompletionFlag complete2(p2, {"completion"});
+
+    args::Command c1(p2, "command1", "desc", [](args::Subparser &sp)
+    {
+        args::ValueFlag<std::string> f1(sp, "name", "description", {'f', "foo"}, "abc");
+        sp.Parse();
+    });
+
+    args::Command c2(p2, "command2", "desc", [](args::Subparser &sp)
+    {
+        args::ValueFlag<std::string> f1(sp, "name", "description", {'b', "bar"}, "abc");
+        sp.Parse();
+    });
+
+    p2.ParseArgs(std::vector<std::string>{"--completion", "bash", "2", "test", "command3", ""});
+    test::require(p2.GetError() == args::Error::Completion);
+    test::require(p2.GetErrorMsg().empty());
+    test::require(args::get(complete2).empty());
     return 0;
 }
